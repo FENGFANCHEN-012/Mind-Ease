bae2046ea07a4706e77a52d96e6e888d6ff0eca8
FENGFANCHEN-012 <Chensail@outlook.com>
2025-12-19 23:03:27 +0800

    # # Changes to be committed: #	new file:   ReactNative/.gitignore #	new file:   ReactNative/.vscode/extensions.json #	new file:   ReactNative/.vscode/settings.json #	new file:   ReactNative/README.md #	new file:   ReactNative/app.json #	new file:   ReactNative/app/(tabs)/_layout.tsx #	new file:   ReactNative/app/(tabs)/chatbot.tsx #	new file:   ReactNative/app/(tabs)/explore.tsx #	new file:   ReactNative/app/(tabs)/index.tsx #	new file:   ReactNative/app/(tabs)/plan.tsx #	new file:   ReactNative/app/(tabs)/profile.tsx #	new file:   ReactNative/app/(tabs)/stress.tsx #	new file:   ReactNative/app/_layout.tsx #	new file:   ReactNative/assets/images/android-icon-background.png #	new file:   ReactNative/assets/images/android-icon-foreground.png #	new file:   ReactNative/assets/images/android-icon-monochrome.png #	new file:   ReactNative/assets/images/favicon.png #	new file:   ReactNative/assets/images/icon.png #	new file:   ReactNative/assets/images/partial-react-logo.png #	new file:   ReactNative/assets/images/react-logo.png #	new file:   ReactNative/assets/images/react-logo@2x.png #	new file:   ReactNative/assets/images/react-logo@3x.png #	new file:   ReactNative/assets/images/splash-icon.png #	new file:   ReactNative/components/external-link.tsx #	new file:   ReactNative/components/haptic-tab.tsx #	new file:   ReactNative/components/hello-wave.tsx #	new file:   ReactNative/components/parallax-scroll-view.tsx #	new file:   ReactNative/components/themed-text.tsx #	new file:   ReactNative/components/themed-view.tsx #	new file:   ReactNative/components/ui/collapsible.tsx #	new file:   ReactNative/components/ui/icon-symbol.ios.tsx #	new file:   ReactNative/components/ui/icon-symbol.tsx #	new file:   ReactNative/constants/theme.ts #	new file:   ReactNative/eslint.config.js #	new file:   ReactNative/hooks/use-color-scheme.ts #	new file:   ReactNative/hooks/use-color-scheme.web.ts #	new file:   ReactNative/hooks/use-theme-color.ts #	new file:   ReactNative/package-lock.json #	new file:   ReactNative/package.json #	new file:   ReactNative/scripts/reset-project.js #	new file:   ReactNative/src/store/hooks.ts #	new file:   ReactNative/src/store/index.ts #	new file:   ReactNative/src/store/mindslice.ts #	new file:   ReactNative/src/store/userSlice.ts #	new file:   ReactNative/tsconfig.json #

---
+++ ReactNative/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../src/store";
+
+export default function RootLayout() {
+  return (
+    <Provider store={store}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="chatbot" />
+      </Stack>
+    </Provider>
+  );
+}
